bd8a98d4b60c20429bf7ac499812ed671f9dcf07
beingshahbaj <130038256+beingshahbaj@users.noreply.github.com>
2023-11-18 15:08:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function enterMetaverse() {
+   
+    let para = document.createElement("p");
+    para.textContent = "Entered Metaverse";
+
+   
+    let parent = document.getElementsByTagName("body")[0];
+    parent.appendChild(para);
+}
